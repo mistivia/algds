357ace94f6bdfbf5a49573c4ff8092089f0b314e
Mistivia <i@mistivia.com>
2026-01-22 06:02:07 +0800

    add alist implementation

---
+++ alist.c
@@ -0,0 +1,13 @@
+#include "alist.h"
+#include "basic_traits.h"
+
+ALIST_IMPL_AS(Int, IntAList)
+ALIST_IMPL_AS(Bool, BoolAList)
+ALIST_IMPL_AS(Long, LongAList)
+ALIST_IMPL_AS(Char, CharAList)
+ALIST_IMPL_AS(UInt, UIntAList)
+ALIST_IMPL_AS(ULong, ULongAList)
+ALIST_IMPL_AS(Double, DoubleAList)
+ALIST_IMPL_AS(Float, FloatAList)
+ALIST_IMPL_AS(String, StringAList)
+ALIST_IMPL_AS(VoidPtr, VoidPtrAList)
