edb60708dc6fdf8c04b6adaec883b31868a117ab
Mistivia <i@mistivia.com>
2026-02-13 18:33:47 +0800

    refactor arc

--- arc.h
+++ arc.h
@@ -4,45 +4,42 @@
 #include <stdatomic.h>
 #include <stdlib.h>
 
-#include <stdatomic.h>
-#include <stdlib.h>
-
 #define ARC(A, T) \
     typedef struct { \
         atomic_int ref_count; \
-        T data; \
-    } A##Ctrl; \
+        T##_t data; \
+    } A##_ctrl_; \
     \
     typedef struct { \
-        T* ptr; \
-        A##Ctrl* ctrl; \
-    } A; \
+        T##_t* ptr; \
+        A##_ctrl_* ctrl; \
+    } A##_t; \
     \
-    A A##_create(T val) __attribute__((weak)); \
-    A A##_copy(A *self) __attribute__((weak)); \
-    void A##_destroy(A *self) __attribute__((weak)); \
-    T* A##_get(A *self) __attribute__((weak)); \
-    A A##_move(A *self) __attribute__((weak)); \
+    A##_t A##_create(T##_t val) __attribute__((weak)); \
+    A##_t A##_copy(A##_t *self) __attribute__((weak)); \
+    void A##_destroy(A##_t *self) __attribute__((weak)); \
+    T##_t* A##_get(A##_t *self) __attribute__((weak)); \
+    A##_t A##_move(A##_t *self) __attribute__((weak)); \
     \
-    A A##_create(T val) { \
-        A##Ctrl* ctrl = (A##Ctrl*)malloc(sizeof(A##Ctrl)); \
+    A##_t A##_create(T##_t val) { \
+        A##_ctrl_* ctrl = (A##_ctrl_*)malloc(sizeof(A##_ctrl_)); \
         atomic_init(&ctrl->ref_count, 1); \
         ctrl->data = val; \
-        A arc = {0}; \
+        A##_t arc = {0}; \
         arc.ptr = &ctrl->data; \
         arc.ctrl = ctrl; \
         return arc; \
     } \
     \
-    A A##_copy(A *self) { \
+    A##_t A##_copy(A##_t *self) { \
         atomic_fetch_add_explicit(&self->ctrl->ref_count, 1, memory_order_relaxed); \
-        A arc = {0}; \
+        A##_t arc = {0}; \
         arc.ptr = self->ptr; \
         arc.ctrl = self->ctrl; \
         return arc; \
     } \
     \
-    void A##_destroy(A *self) { \
+    void A##_destroy(A##_t *self) { \
         if (self->ctrl == NULL) return; \
         if (atomic_fetch_sub_explicit(&self->ctrl->ref_count, 1, memory_order_acq_rel) == 1) { \
             free(self->ctrl); \
@@ -51,12 +48,12 @@
         self->ctrl = NULL; \
     } \
     \
-    T* A##_get(A *self) { \
+    T##_t* A##_get(A##_t *self) { \
         return self->ptr; \
     } \
     \
-    A A##_move(A *self) { \
-        A arc = *self; \
+    A##_t A##_move(A##_t *self) { \
+        A##_t arc = *self; \
         self->ptr = NULL; \
         self->ctrl = NULL; \
         return arc; \
--- tests/test_arc.c
+++ tests/test_arc.c
@@ -2,106 +2,107 @@
 #include <stdio.h>
 #include "arc.h"
 
-/* 测试int类型的ARC */
-ARC(IntArc, int)
 
-/* 测试结构体类型的ARC */
+/* 测试结构体类型 */
 typedef struct {
-    int x;
-    int y;
-} Point;
-ARC(PointArc, Point)
+    int32_t x;
+    int32_t y;
+} point_t;
+
+/* 实例化ARC宏 */
+ARC(int_arc, int32)
+ARC(point_arc, point)
 
 void test_basic_create_destroy() {
-    IntArc arc = IntArc_create(42);
-    assert(*IntArc_get(&arc) == 42);
+    int_arc_t arc = int_arc_create(42);
+    assert(*int_arc_get(&arc) == 42);
     assert(atomic_load_explicit(&arc.ctrl->ref_count, memory_order_relaxed) == 1);
-    IntArc_destroy(&arc);
+    int_arc_destroy(&arc);
     printf("test_basic_create_destroy passed\n");
 }
 
 void test_copy_increments_count() {
-    IntArc arc1 = IntArc_create(100);
-    IntArc arc2 = IntArc_copy(&arc1);
+    int_arc_t arc1 = int_arc_create(100);
+    int_arc_t arc2 = int_arc_copy(&arc1);
     
-    assert(*IntArc_get(&arc1) == 100);
-    assert(*IntArc_get(&arc2) == 100);
+    assert(*int_arc_get(&arc1) == 100);
+    assert(*int_arc_get(&arc2) == 100);
     assert(atomic_load_explicit(&arc1.ctrl->ref_count, memory_order_relaxed) == 2);
     assert(arc1.ctrl == arc2.ctrl);  // 共享同一控制块
     
-    IntArc_destroy(&arc1);
+    int_arc_destroy(&arc1);
     // arc1已无效，但arc2仍应有效，引用计数为1
-    assert(*IntArc_get(&arc2) == 100);
+    assert(*int_arc_get(&arc2) == 100);
     assert(atomic_load_explicit(&arc2.ctrl->ref_count, memory_order_relaxed) == 1);
     
-    IntArc_destroy(&arc2);
+    int_arc_destroy(&arc2);
     printf("test_copy_increments_count passed\n");
 }
 
 void test_multiple_copies() {
-    IntArc arc1 = IntArc_create(10);
-    IntArc arc2 = IntArc_copy(&arc1);
-    IntArc arc3 = IntArc_copy(&arc2);
+    int_arc_t arc1 = int_arc_create(10);
+    int_arc_t arc2 = int_arc_copy(&arc1);
+    int_arc_t arc3 = int_arc_copy(&arc2);
     
     assert(atomic_load(&arc1.ctrl->ref_count) == 3);
     
-    IntArc_destroy(&arc1);
+    int_arc_destroy(&arc1);
     assert(atomic_load(&arc2.ctrl->ref_count) == 2);
     
-    IntArc_destroy(&arc2);
+    int_arc_destroy(&arc2);
     assert(atomic_load(&arc3.ctrl->ref_count) == 1);
     
-    IntArc_destroy(&arc3);
+    int_arc_destroy(&arc3);
     printf("test_multiple_copies passed\n");
 }
 
 void test_struct_data() {
-    Point p = {3, 4};
-    PointArc arc = PointArc_create(p);
+    point_t p = {3, 4};
+    point_arc_t arc = point_arc_create(p);
     
-    assert(PointArc_get(&arc)->x == 3);
-    assert(PointArc_get(&arc)->y == 4);
+    assert(point_arc_get(&arc)->x == 3);
+    assert(point_arc_get(&arc)->y == 4);
     
     // 修改通过指针应影响原始数据
-    PointArc_get(&arc)->x = 10;
-    assert(PointArc_get(&arc)->x == 10);
+    point_arc_get(&arc)->x = 10;
+    assert(point_arc_get(&arc)->x == 10);
     
-    PointArc arc2 = PointArc_copy(&arc);
+    point_arc_t arc2 = point_arc_copy(&arc);
     // 共享数据，修改arc2会影响arc
-    PointArc_get(&arc2)->y = 20;
-    assert(PointArc_get(&arc)->y == 20);
+    point_arc_get(&arc2)->y = 20;
+    assert(point_arc_get(&arc)->y == 20);
     
-    PointArc_destroy(&arc);
-    PointArc_destroy(&arc2);
+    point_arc_destroy(&arc);
+    point_arc_destroy(&arc2);
     printf("test_struct_data passed\n");
 }
 
 void test_move_semantics() {
-    IntArc arc1 = IntArc_create(99);
+    int_arc_t arc1 = int_arc_create(99);
     assert(atomic_load(&arc1.ctrl->ref_count) == 1);
     
-    IntArc arc2 = IntArc_move(&arc1);
+    int_arc_t arc2 = int_arc_move(&arc1);
     
     // arc1应被置空
     assert(arc1.ptr == NULL);
     assert(arc1.ctrl == NULL);
     
     // arc2拥有资源，引用计数不变
-    assert(*IntArc_get(&arc2) == 99);
+    assert(*int_arc_get(&arc2) == 99);
     assert(atomic_load(&arc2.ctrl->ref_count) == 1);
     
     // 安全销毁空arc1（不应崩溃）
-    IntArc_destroy(&arc1);
+    int_arc_destroy(&arc1);
     
-    IntArc_destroy(&arc2);
+    int_arc_destroy(&arc2);
     printf("test_move_semantics passed\n");
 }
 
 void test_double_destroy_safe() {
-    IntArc arc = IntArc_create(5);
-    IntArc_destroy(&arc);
+    int_arc_t arc = int_arc_create(5);
+    int_arc_destroy(&arc);
     // 再次销毁不应崩溃（有NULL检查）
-    IntArc_destroy(&arc);
+    int_arc_destroy(&arc);
     printf("test_double_destroy_safe passed\n");
 }
 
